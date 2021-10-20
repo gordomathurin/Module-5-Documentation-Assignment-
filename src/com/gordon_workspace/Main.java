450e65622fcc170f776afa6c886af161ef17ec2b
gordomathurin <mathuringordon@gmail.com>
2021-10-20 16:00:05 -0400

    fib class created and running, chart of results added, generated javaDoc

--- src/com/gordon_workspace/Main.java
+++ src/com/gordon_workspace/Main.java
@@ -3,6 +3,28 @@ package com.gordon_workspace;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        long startTime, endTime;
+        int runTime;
+        Fibonacci fibonacci = new Fibonacci();
+
+        for (int i = 1; i <= 10; i++) {
+            startTime = System.nanoTime();
+            int xx = fibonacci.fibRecursive(i);
+            endTime = System.nanoTime();
+            runTime = (int)((endTime - startTime));
+            System.out.println("Recursion answer is " + xx + "\t\t" + "Run time for function was: " + runTime);
+        }
+
+       fibonacci.separator();
+
+        for (int i = 1; i <= 10; i++ ) {
+            startTime = System.nanoTime();
+            int z = fibonacci.fibIterative(i);
+            endTime = System.nanoTime();
+            runTime = (int)((endTime - startTime));
+            System.out.println("Iterative answer is " + z + "\t\t" + "Run time for function was: " + runTime);
+        }
+
     }
 }
---
+++ src/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.gordon_workspace","l":"Fibonacci"},{"p":"com.gordon_workspace","l":"Main"}]
